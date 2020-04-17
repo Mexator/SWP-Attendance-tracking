935a47bbb3bad20df558f94db4395f3fb74d6413
Cendien <ruslan201390@users.noreply.github.com>
2020-04-17 16:28:43 +0300

    New layouts and java classes for them for bluetooth gathering. Some changes in navigation graph.

---
+++ app/src/main/java/com/example/attendancelogger/BluetoothMarkingFragment.java
@@ -0,0 +1,90 @@
+package com.example.attendancelogger;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class BluetoothMarkingFragment extends Fragment implements View.OnClickListener, Response.Listener<JSONObject>, Response.ErrorListener {
+    private EditText classIdEdit, activityIdEdit, weekEdit;
+    private AttendanceBackend backend;
+    private View progressBar;
+
+    public BluetoothMarkingFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        backend = AttendanceBackend.getInstance(getContext());
+        return inflater.inflate(R.layout.fragment_try_mark_me_on_this_subject, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        classIdEdit = view.findViewById(R.id.class_id_edit);
+        activityIdEdit=view.findViewById(R.id.activity_id_edit);
+        weekEdit = view.findViewById(R.id.week_edit);
+
+        view.findViewById(R.id.confirm_marking_button).setOnClickListener(this);
+
+        progressBar = new ProgressBar(getActivity(), null, android.R.attr.progressBarStyleLarge);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(100, 100);
+        params.addRule(RelativeLayout.CENTER_IN_PARENT);
+        LinearLayout l = getView().findViewById(R.id.student_bluetooth_marking_layout);
+        l.addView(progressBar, params);
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.confirm_marking_button:
+                progressBar.setVisibility(View.VISIBLE);
+                Long classId = Long.parseLong(classIdEdit.getText().toString());
+                Long activityId = Long.parseLong(activityIdEdit.getText().toString());
+
+                Long userId;
+                userId = backend.getUser().getID();
+
+                Integer weekNumber = Integer.parseInt(weekEdit.getText().toString());
+                try {
+                    backend.sendPresenceRequest(
+                            classId,activityId,userId,weekNumber,this,this);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        progressBar.setVisibility(View.INVISIBLE);
+        Toast.makeText(getContext(),"Successfully marked!",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(getContext(),"Error",Toast.LENGTH_SHORT).show();
+    }
+}
