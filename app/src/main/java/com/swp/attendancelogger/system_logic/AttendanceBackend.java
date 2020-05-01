b955075fdd82291e8abbf53bde49594f5f32f85a
Anton Brisilin <a.brisilin@innopolis.university>
2020-05-01 20:55:30 +0300

    add some solution

--- app/src/main/java/com/swp/attendancelogger/system_logic/AttendanceBackend.java
+++ app/src/main/java/com/swp/attendancelogger/system_logic/AttendanceBackend.java
@@ -2,11 +2,13 @@ package com.swp.attendancelogger.system_logic;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -23,11 +25,14 @@ import java.util.Map;
 /**
  * Represents API to interact with backend. Uses Singleton pattern
  */
-public class AttendanceBackend{
+public class AttendanceBackend {
     private static AttendanceBackend instance;
     private User user;
     private String APIEndpoint;
     private RequestQueue requestQueue;
+
+    private final long RENEWAL_INTERVAL = 1000 * 60 * 60 / 2;
+    private final String RENEWAL_ENDPOINT = "session/renew";
 
     private String token,
             renewalToken;
@@ -41,8 +46,8 @@ public class AttendanceBackend{
     }
 
     public static AttendanceBackend getInstance(Context context) {
-        if (instance == null){
-            synchronized (AttendanceBackend.class){
+        if (instance == null) {
+            synchronized (AttendanceBackend.class) {
                 instance = new AttendanceBackend(context);
             }
         }
@@ -66,16 +71,17 @@ public class AttendanceBackend{
         setToken(token, renewalToken, lastUpdate);
     }
 
-    public void setToken(String token, String renewalToken, Date lastUpdate){
+    public void setToken(String token, String renewalToken, Date lastUpdate) {
         this.token = token;
         this.renewalToken = renewalToken;
         this.lastUpdate = lastUpdate;
     }
+
     public void setUser(User user) {
         this.user = user;
     }
 
-    public void saveAccessInfo(Context context){
+    public void saveAccessInfo(Context context) {
         synchronized (AttendanceBackend.class) {
             SharedPreferences prefs = context.getSharedPreferences(
                     SharedConstants.ACCESS_PREFS_FILE,
@@ -92,6 +98,7 @@ public class AttendanceBackend{
 
     /**
      * Sends request for receiving auth token, as described in the API
+     *
      * @param username      same as in API description
      * @param password      same as in API description
      * @param listener      callback to handle response
@@ -104,69 +111,105 @@ public class AttendanceBackend{
         JSONObject request = new JSONObject();
         JSONObject user = new JSONObject();
         user.put("email", username);
-        user.put("password",password);
-        request.put("user",user);
+        user.put("password", password);
+        request.put("user", user);
 
-        String url = APIEndpoint +"session/";
-        sendRequest(Request.Method.POST,url,request,listener,errorListener);
+        String url = APIEndpoint + "session/";
+        sendRequest(Request.Method.POST, url, request, listener, errorListener);
     }
 
     /**
      * Sends request to get current user
+     *
      * @param listener      callback to handle response
      * @param errorListener callback to handle errors
      * @throws JSONException
      */
     public void sendUserRequest(Response.Listener<JSONObject> listener,
-                                Response.ErrorListener errorListener){
+                                Response.ErrorListener errorListener) {
         assert token != null;
-        sendRequest(Request.Method.GET, APIEndpoint +"current_user",null,listener,errorListener);
+        sendRequest(Request.Method.GET, APIEndpoint + "current_user", null, listener, errorListener);
     }
 
     /**
      * Sends request for receiving auth token, as described in the API
-     * @param classId    same as in API description
-     * @param activityId same as in API description
-     * @param userId     same as in API description
-     * @param week       same as in API description
+     *
+     * @param classId       same as in API description
+     * @param activityId    same as in API description
+     * @param userId        same as in API description
+     * @param week          same as in API description
      * @param listener      callback to handle response
      * @param errorListener callback to handle errors
      * @throws JSONException
      */
     public void sendPresenceRequest(Long classId, Long activityId, Long userId, Integer week,
                                     Response.Listener<JSONObject> listener,
-                                    Response.ErrorListener errorListener) throws JSONException{
+                                    Response.ErrorListener errorListener) throws JSONException {
 
         JSONObject body = new JSONObject();
         JSONObject presence = new JSONObject();
         presence.put("class_id", classId);
-        presence.put("activity_id",activityId);
-        presence.put("user_id",userId);
-        presence.put("week",week);
-        body.put("presence",presence);
+        presence.put("activity_id", activityId);
+        presence.put("user_id", userId);
+        presence.put("week", week);
+        body.put("presence", presence);
 
-        String url = APIEndpoint +"presences/";
-        sendRequest(Request.Method.POST,url,body,listener,errorListener);
+        String url = APIEndpoint + "presences/";
+        sendRequest(Request.Method.POST, url, body, listener, errorListener);
     }
 
     /**
      * Sends request to get list of classes
+     *
      * @param listener      callback to handle response
      * @param errorListener callback to handle errors
      */
     public void sendClassesRequest(Response.Listener<JSONObject> listener,
-                               Response.ErrorListener errorListener){
+                                   Response.ErrorListener errorListener) {
         String url = APIEndpoint + "user_classes";
-        sendRequest(Request.Method.GET,url,null,listener,errorListener);
-    }
-    public void sendUsersByClassRequest(Long classID,Response.Listener<JSONObject> listener,
-                                        Response.ErrorListener errorListener) {
-        String url = APIEndpoint + "class_students/" + classID;
-        sendRequest(Request.Method.GET, url,null,listener,errorListener);
+        sendRequest(Request.Method.GET, url, null, listener, errorListener);
     }
 
+    public void sendUsersByClassRequest(Long classID, Response.Listener<JSONObject> listener,
+                                        Response.ErrorListener errorListener) {
+        String url = APIEndpoint + "class_students/" + classID;
+        sendRequest(Request.Method.GET, url, null, listener, errorListener);
+    }
+    private void sendRenewalRequest(){
+        JsonObjectRequest renewalRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                APIEndpoint + RENEWAL_ENDPOINT,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject data = response.getJSONObject("data");
+                            setToken(data.getString("token"),
+                                    data.getString("renewal_token"));
+                        } catch (JSONException e) {
+                            Log.e("", e.toString());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("",error.toString());
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Authorization", renewalToken);
+                return headers;
+            }
+        };
+        requestQueue.add(renewalRequest);
+    }
     /**
      * General purpose method to send any request
+     *
      * @param method        POST, GET, etc.
      * @param url           target url
      * @param body          json body
@@ -175,22 +218,27 @@ public class AttendanceBackend{
      */
     private void sendRequest(int method, String url, JSONObject body,
                              Response.Listener<JSONObject> listener,
-                             Response.ErrorListener errorListener){
+                             Response.ErrorListener errorListener) {
         //TODO Add token renewal
+        if ((new Date().getTime() - lastUpdate.getTime()) > RENEWAL_INTERVAL) {
+            sendRenewalRequest();
+        }
         JsonObjectRequest request;
         if (token != null) {
             request = new JsonObjectRequest(method, url, body, listener, errorListener) {
                 @Override
-                public Map getHeaders() throws AuthFailureError {
+                public Map<String, String> getHeaders() throws AuthFailureError {
                     HashMap<String, String> headers = new HashMap<>();
                     headers.put("Authorization", token);
                     return headers;
                 }
             };
-        }
-        else{
+        } else {
             request = new JsonObjectRequest(method, url, body, listener, errorListener);
         }
         requestQueue.add(request);
     }
 }
+
+
+//curl -X POST -H "Authorization: RENEW_TOKEN" https://attendance-inno.herokuapp.com/api/api/session/renew
